84b1e9eaeec9de069b59e660d0b1a5e5084078b5
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-04-21 11:32:37 +0200

    feat: add shuffle cards logic

--- src/App.jsx
+++ src/App.jsx
@@ -56,9 +56,31 @@ function App() {
       console.log('game over');
     } else {
       setSelectedCards([...selectedCards, key]);
+      setCharacters(shuffleCards(characters));
     }
   }
   console.log('Selected Cards', selectedCards);
+
+  // shuffle cards
+  function shuffleCards(arrayToShuffle) {
+    let array = arrayToShuffle;
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
 
   return (
     <>

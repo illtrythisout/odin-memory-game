cbae71abf2a44c11e54107b92f618e5a1227a7ab
Theo <170449321+illtrythisout@users.noreply.github.com>
2025-04-21 09:08:03 +0200

    feat: create gameboard components and style them

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import './styles/base.css'
+import './styles/base.css';
+import { useState } from 'react';
+import GameBoard from './components/Game-Board';
 
 function App() {
+  const images = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const [selectedCards, setSelectedCards] = useState([]);
+
+  console.log(selectedCards);
+
+  function selectCard(key) {
+    if (selectedCards.includes(key)) {
+      console.log('game over');
+    } else {
+      setSelectedCards([...selectedCards, key]);
+    }
+  }
+
   return (
     <>
-      <main></main>
+      <main>
+        <GameBoard images={images} selectCard={selectCard} />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Game-Board.jsx
@@ -0,0 +1,14 @@
+import '../styles/game-board.css';
+import '../styles/card.css';
+
+function GameBoard() {
+  return (
+    <div className="game-board">
+      {/* {images.map((image) => {
+        return <Card />;
+      })} */}
+    </div>
+  );
+}
+
+export default GameBoard;
